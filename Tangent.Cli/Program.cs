724379c41bdf46ac4cce1db9a2ebb80fc35d7d67
Telastyn <rms.tangent@gmail.com>
2014-11-26 14:03:31 -0600

    RMS: basic front end projects.

---
+++ Tangent.Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tangent.Cli {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
