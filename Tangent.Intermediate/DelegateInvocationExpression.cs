053a7f5a0a074517e66f0bde3f5a5d4087577ef7
Telastyn <rms.tangent@gmail.com>
2015-01-29 08:38:23 -0600

    RMS: missing file from last commit.

---
+++ Tangent.Intermediate/DelegateInvocationExpression.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangent.Intermediate
+{
+    public class DelegateInvocationExpression : Expression
+    {
+        public readonly Expression Delegate;
+
+        public override ExpressionNodeType NodeType
+        {
+            get { return ExpressionNodeType.DelegateInvocation; }
+        }
+
+        public DelegateInvocationExpression(Expression d)
+        {
+            Delegate = d;
+        }
+
+        public TangentType EffectiveType
+        {
+            get
+            {
+                switch (Delegate.NodeType) {
+                    case ExpressionNodeType.ParameterAccess:
+                        var parameter = Delegate as ParameterAccessExpression;
+                        var lazyType = parameter.Parameter.Returns as LazyType;
+                        return lazyType.Type;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
