b9e157acfe36e023193aa43792ab68decafe63e4
Telastyn <rms.tangent@gmail.com>
2015-01-31 08:50:11 -0600

    RMS: missing file from last commit.

---
+++ Tangent.CilGeneration/IFunctionLookup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tangent.Intermediate;
+
+namespace Tangent.CilGeneration
+{
+    public interface IFunctionLookup
+    {
+        MethodInfo this[ReductionDeclaration fn] { get; }
+    }
+}
