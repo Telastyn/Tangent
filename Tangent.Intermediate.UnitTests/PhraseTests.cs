9d9d0d46414cb47f819cb693cb1394b446ea6dd6
Telastyn <rms.tangent@gmail.com>
2015-11-25 08:03:28 -0600

    RMS: adding matching logic to phrases. Including incoming params as part of the matching result.

---
+++ Tangent.Intermediate.UnitTests/PhraseTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tangent.Intermediate.UnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class PhraseTests
+    {
+        [TestMethod]
+        public void HappyPath()
+        {
+            var genericT = new ParameterDeclaration("T", TangentType.Any.Kind);
+            var phrase = new Phrase(new[]{
+                new PhrasePart(new Identifier("foo")),
+                new PhrasePart(new ParameterDeclaration("x", TangentType.Int)),
+                new PhrasePart(new ParameterDeclaration("y", GenericInferencePlaceholder.For(genericT)))});
+
+            var input = new Expression[]{ 
+                new IdentifierExpression(new Identifier("foo"), null),
+                new ConstantExpression<int>(TangentType.Int, 42, null),
+                new ConstantExpression<string>(TangentType.String, "moo", null)};
+
+            var result = phrase.TryMatch(input);
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual(2, result.IncomingParameters.Count());
+            Assert.AreEqual(1, result.GenericInferences.Count());
+
+            Assert.AreEqual(genericT, result.GenericInferences.First().Key);
+            Assert.AreEqual(TangentType.String, result.GenericInferences.First().Value);
+
+            Assert.IsTrue(result.IncomingParameters.First() is ConstantExpression<int>);
+            Assert.IsTrue(result.IncomingParameters.Skip(1).First() is ConstantExpression<string>);
+        }
+
+        [TestMethod]
+        public void PartialMatchesAreFine()
+        {
+            var genericT = new ParameterDeclaration("T", TangentType.Any.Kind);
+            var phrase = new Phrase(new[]{
+                new PhrasePart(new Identifier("foo")),
+                new PhrasePart(new ParameterDeclaration("x", TangentType.Int)),
+                new PhrasePart(new ParameterDeclaration("y", GenericInferencePlaceholder.For(genericT)))});
+
+            var input = new Expression[]{ 
+                new IdentifierExpression(new Identifier("foo"), null),
+                new ConstantExpression<int>(TangentType.Int, 42, null),
+                new ConstantExpression<string>(TangentType.String, "moo", null),
+                new IdentifierExpression(new Identifier("fooooo"), null)
+            };
+
+            var result = phrase.TryMatch(input);
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual(2, result.IncomingParameters.Count());
+            Assert.AreEqual(1, result.GenericInferences.Count());
+
+            Assert.AreEqual(genericT, result.GenericInferences.First().Key);
+            Assert.AreEqual(TangentType.String, result.GenericInferences.First().Value);
+
+            Assert.IsTrue(result.IncomingParameters.First() is ConstantExpression<int>);
+            Assert.IsTrue(result.IncomingParameters.Skip(1).First() is ConstantExpression<string>);
+        }
+
+        [TestMethod]
+        public void ShortInputFailsGracefully()
+        {
+            var genericT = new ParameterDeclaration("T", TangentType.Any.Kind);
+            var phrase = new Phrase(new[]{
+                new PhrasePart(new Identifier("foo")),
+                new PhrasePart(new ParameterDeclaration("x", TangentType.Int)),
+                new PhrasePart(new ParameterDeclaration("y", GenericInferencePlaceholder.For(genericT)))});
+
+            var input = new Expression[]{ 
+                new IdentifierExpression(new Identifier("foo"), null),
+                new ConstantExpression<int>(TangentType.Int, 42, null)};
+
+            var result = phrase.TryMatch(input);
+
+            Assert.IsFalse(result.Success);
+        }
+
+        [TestMethod]
+        public void ParamMismatchFailsGracefully()
+        {
+            var genericT = new ParameterDeclaration("T", TangentType.Any.Kind);
+            var phrase = new Phrase(new[]{
+                new PhrasePart(new Identifier("foo")),
+                new PhrasePart(new ParameterDeclaration("x", TangentType.Int)),
+                new PhrasePart(new ParameterDeclaration("y", GenericInferencePlaceholder.For(genericT)))});
+
+            var input = new Expression[]{ 
+                new IdentifierExpression(new Identifier("foo"), null),
+                new ConstantExpression<string>(TangentType.String, "42", null),
+                new ConstantExpression<string>(TangentType.String, "moo", null)};
+
+            var result = phrase.TryMatch(input);
+
+            Assert.IsFalse(result.Success);
+        }
+
+        [TestMethod]
+        public void ParamMismatchFailsGracefully2()
+        {
+            var genericT = new ParameterDeclaration("T", TangentType.Any.Kind);
+            var phrase = new Phrase(new[]{
+                new PhrasePart(new Identifier("foo")),
+                new PhrasePart(new ParameterDeclaration("x", TangentType.Int)),
+                new PhrasePart(new ParameterDeclaration("y", GenericInferencePlaceholder.For(genericT)))});
+
+            var input = new Expression[]{ 
+                new IdentifierExpression(new Identifier("foo"), null),
+                new ConstantExpression<int>(TangentType.Int, 42, null),
+                new IdentifierExpression(new Identifier("foo"), null)};
+
+            var result = phrase.TryMatch(input);
+
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
--- Tangent.Intermediate/PhraseMatchResult.cs
+++ Tangent.Intermediate/PhraseMatchResult.cs
@@ -9,6 +9,7 @@ namespace Tangent.Intermediate
     public class PhraseMatchResult
     {
         public readonly int Length;
+        public readonly IEnumerable<Expression> IncomingParameters;
         public readonly Dictionary<ParameterDeclaration, TangentType> GenericInferences;
         public bool Success
         {
@@ -21,8 +22,9 @@ namespace Tangent.Intermediate
         public static readonly PhraseMatchResult Failure = new PhraseMatchResult();
 
         private PhraseMatchResult() { }
-        public PhraseMatchResult(int length, Dictionary<ParameterDeclaration, TangentType> genericInferences = null)
+        public PhraseMatchResult(int length, IEnumerable<Expression> matchedParameters = null, Dictionary<ParameterDeclaration, TangentType> genericInferences = null)
         {
+            IncomingParameters = matchedParameters ?? Enumerable.Empty<Expression>();
             GenericInferences = genericInferences == null ? new Dictionary<ParameterDeclaration, TangentType>() : new Dictionary<ParameterDeclaration, TangentType>(genericInferences);
             Length = length;
         }
