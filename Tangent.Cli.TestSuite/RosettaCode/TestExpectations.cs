631c57d0b7c2ad3bbe523921002ab43048c26a88
Telastyn <rms.tangent@gmail.com>
2017-04-26 14:51:01 -0500

    RMS: broken rosetta code.

--- Tangent.Cli.TestSuite/RosettaCode/TestExpectations.cs
+++ Tangent.Cli.TestSuite/RosettaCode/TestExpectations.cs
@@ -14,7 +14,7 @@ namespace Tangent.Cli.TestSuite.RosettaCode
         [TestMethod]
         public void AplusB()
         {
-            var result = Test.ProgramFile(new[] { @"RosettaCode\AplusB.tan", @"lib\conditional-lib.tan", @"lib\looping-lib.tan", @"lib\console-lib.tan" , @"lib\string-lib.tan", @"lib\enumerable-lib.tan"}, new[] { typeof(IEnumerable<>).Assembly, typeof(List<>).Assembly, typeof(Enumerable).Assembly });
+            var result = Test.ProgramFile(new[] { @"RosettaCode\AplusB.tan", @"lib\conditional-lib.tan", @"lib\looping-lib.tan", @"lib\console-lib.tan", @"lib\string-lib.tan", @"lib\enumerable-lib.tan" }, new[] { typeof(IEnumerable<>).Assembly, typeof(List<>).Assembly, typeof(Enumerable).Assembly });
             var results = result.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
             Assert.IsTrue(results.SequenceEqual(new string[] { }));
         }
@@ -31,10 +31,32 @@ namespace Tangent.Cli.TestSuite.RosettaCode
         [TestMethod]
         public void Factorial()
         {
-            var result = Test.DebugProgramFile(new[] { @"RosettaCode\factorial.tan", @"lib\conditional-lib.tan",  @"lib\console-lib.tan" }, new[] { typeof(Console).Assembly, typeof(TextReader).Assembly },
+            var result = Test.DebugProgramFile(new[] { @"RosettaCode\factorial.tan", @"lib\conditional-lib.tan", @"lib\console-lib.tan" }, new[] { typeof(Console).Assembly, typeof(TextReader).Assembly },
                 "9");
             var results = result.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
             Assert.IsTrue(results.SequenceEqual(new string[] { "362880" }));
+        }
+
+        [Ignore] // RMS: compilation takes minutes... TODO.
+        [TestMethod]
+        public void FizzBuzz()
+        {
+            var result = Test.DebugProgramFile(new[] { @"RosettaCode\fizzbuzz.tan", @"lib\conditional-lib.tan", @"lib\looping-lib.tan", @"lib\console-lib.tan", @"lib\string-lib.tan" }, new[] { typeof(string).Assembly });
+            var results = result.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
+            Assert.IsTrue(results.SequenceEqual(FizzBuzzResult));
+        }
+
+        private IEnumerable<string> FizzBuzzResult
+        {
+            get
+            {
+                for (int x = 1; x <= 100; ++x) {
+                    string s = "";
+                    if (x % 3 == 0) { s += "Fizz"; }
+                    if (x % 5 == 0) { s += "Buzz"; }
+                    if (string.IsNullOrWhiteSpace(s)) { yield return x.ToString(); } else { yield return s; }
+                }
+            }
         }
     }
 }
